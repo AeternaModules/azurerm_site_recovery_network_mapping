839e026e750379da648fdda8c318165f12a55fc9
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "site_recovery_network_mappings" {
-  description = "All site_recovery_network_mapping resources"
-  value       = azurerm_site_recovery_network_mapping.site_recovery_network_mappings
-}
 output "site_recovery_network_mappings_name" {
-  description = "List of name values across all site_recovery_network_mappings"
-  value       = [for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : v.name]
+  description = "Map of name values across all site_recovery_network_mappings, keyed the same as var.site_recovery_network_mappings"
+  value       = { for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : k => v.name }
 }
 output "site_recovery_network_mappings_recovery_vault_name" {
-  description = "List of recovery_vault_name values across all site_recovery_network_mappings"
-  value       = [for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : v.recovery_vault_name]
+  description = "Map of recovery_vault_name values across all site_recovery_network_mappings, keyed the same as var.site_recovery_network_mappings"
+  value       = { for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : k => v.recovery_vault_name }
 }
 output "site_recovery_network_mappings_resource_group_name" {
-  description = "List of resource_group_name values across all site_recovery_network_mappings"
-  value       = [for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : v.resource_group_name]
+  description = "Map of resource_group_name values across all site_recovery_network_mappings, keyed the same as var.site_recovery_network_mappings"
+  value       = { for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : k => v.resource_group_name }
 }
 output "site_recovery_network_mappings_source_network_id" {
-  description = "List of source_network_id values across all site_recovery_network_mappings"
-  value       = [for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : v.source_network_id]
+  description = "Map of source_network_id values across all site_recovery_network_mappings, keyed the same as var.site_recovery_network_mappings"
+  value       = { for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : k => v.source_network_id }
 }
 output "site_recovery_network_mappings_source_recovery_fabric_name" {
-  description = "List of source_recovery_fabric_name values across all site_recovery_network_mappings"
-  value       = [for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : v.source_recovery_fabric_name]
+  description = "Map of source_recovery_fabric_name values across all site_recovery_network_mappings, keyed the same as var.site_recovery_network_mappings"
+  value       = { for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : k => v.source_recovery_fabric_name }
 }
 output "site_recovery_network_mappings_target_network_id" {
-  description = "List of target_network_id values across all site_recovery_network_mappings"
-  value       = [for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : v.target_network_id]
+  description = "Map of target_network_id values across all site_recovery_network_mappings, keyed the same as var.site_recovery_network_mappings"
+  value       = { for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : k => v.target_network_id }
 }
 output "site_recovery_network_mappings_target_recovery_fabric_name" {
-  description = "List of target_recovery_fabric_name values across all site_recovery_network_mappings"
-  value       = [for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : v.target_recovery_fabric_name]
+  description = "Map of target_recovery_fabric_name values across all site_recovery_network_mappings, keyed the same as var.site_recovery_network_mappings"
+  value       = { for k, v in azurerm_site_recovery_network_mapping.site_recovery_network_mappings : k => v.target_recovery_fabric_name }
 }
 
